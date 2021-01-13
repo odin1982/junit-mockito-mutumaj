ce46f3853568e17b633132c3f4313ad23e7a4442
odinPcWin10 <odin.araujo.barragan@gmail.com>
2021-01-13 14:54:54 -0600

    Se suben cambios de estudio junit

---
+++ src/test/java/org/junit/recap/AssertTest.java
@@ -0,0 +1,54 @@
+package org.junit.recap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+public class AssertTest {
+	
+	@Test
+	public void assertTrueAndFalseTest() throws Exception{
+		Assert.assertTrue(true);
+		Assert.assertFalse(false);
+	}
+	
+	@Test
+	public void asserttNullAndNotNullTest() throws Exception{
+		Object myObject = null;
+		Assert.assertNull(myObject);
+		
+		myObject = new String("Some value");
+		Assert.assertNotNull(myObject);
+	}
+	
+	@Test
+	public void assertEqualsTest() throws Exception{
+		Integer i = new Integer("5");
+		Integer j = new Integer("5");
+		assertEquals(i, j);
+	}
+	
+	@Test
+	public void assertNotSameTest() throws Exception{
+		Integer i = new Integer("5");
+		Integer j = new Integer("5");
+		assertNotSame(i, j);
+	}
+	
+	@Test
+	public void assertSameTest() throws Exception{
+		Integer i = new Integer("5");
+		Integer j = i;
+		assertSame(i, j);
+	}
+	
+	@Test
+	@Ignore("John's holiday stuff failing")
+	public void when_today_is_holiday_then_stop_alarm() {
+		
+	}
+	
+}
